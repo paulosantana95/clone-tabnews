06ea9d06c351329aebf9fe00be5fc1fd5de94656
Paulo Santana <91387292+paulosantana95@users.noreply.github.com>
2023-07-03 01:41:19 +0000

    Adiciona scripts `lint:check`, `lint:fix` e `.prettierignore`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório criado para o curso do Felipe Deschamps.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-  return <h1>Pena de urubu, pena de Galinha, se você vai me dar uns beijos dê uma risadinha. (APENAS PARA RANNAH DANTAS)</h1>
+  return (
+    <h1>
+      Pena de urubu, pena de Galinha, se você vai me dar uns beijos dê uma
+      risadinha. (APENAS PARA RANNAH DANTAS)
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
