9f03a2deb37b932c246cd8a5d8180b65fac194f7
Paulo Santana <paulosantana.sa@gmail.com>
2025-08-10 13:32:23 -0300

    adds `/api/v1/migrations` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 import database from "infra/database.js";
 
-async function status(request, response) {
+export default async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
@@ -29,5 +29,3 @@ async function status(request, response) {
     },
   });
 }
-
-export default status;
