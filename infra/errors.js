2ef710400868fee89cd20484ddc71c9ff0a66c89
Paulo Santana <paulosantana.sa@gmail.com>
2025-10-15 00:25:15 -0300

    feat: create and use `ValidationError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -45,9 +45,30 @@ export class ServiceError extends Error {
       cause,
     });
 
-    this.name = "InternalServerError";
+    this.name = "ServiceError";
     this.action = "Verifique se o serviço está disponível.";
     this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Um erro de validação ocorreu.", {
+      cause,
+    });
+
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.statusCode = 400;
   }
 
   toJSON() {
